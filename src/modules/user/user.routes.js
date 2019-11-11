3a3eede74e9723438b90cb8d904025c4804cffba
SIndujan28 <30890973+SIndujan28@users.noreply.github.com>
2019-11-11 23:59:11 +0530

    feature:added oauth

--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import * as userController from './user.controller';
-import { authLocalCustomer } from './../../services/auth.service';
+import { authLocalCustomer, authFacebook, authGoogle, authTwitter } from './../../services/auth.service';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => res.send('ollla amigos'));
 routes.post('/signup', userController.Signup);
 routes.post('/login', authLocalCustomer, userController.login);
+routes.get('/oauth/google', authGoogle);
+routes.get('/oauth/google/redirect', authGoogle, userController.googleOAuth);
+routes.get('/oauth/twitter', authTwitter);
+routes.get('/oauth/twitter/redirect', authTwitter, userController.facebookOAuth);
+routes.get('/oauth/facebook', authFacebook);
+routes.get('/oauth/facebook/redirect', authFacebook, userController.facebookOAuth);
 export default routes;
