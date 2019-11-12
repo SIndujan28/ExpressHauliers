15cc1831f1999ab6699ec8b65fc181473f2f90bd
SIndujan28 <30890973+SIndujan28@users.noreply.github.com>
2019-11-13 00:38:56 +0530

    added field validation for User model

--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -1,18 +1,28 @@
 import { Router } from 'express';
+import validate from 'express-validation';
 import * as userController from './user.controller';
+import userValidation from './user.validation';
 import { authLocalCustomer, authFacebook, authGoogle, authTwitter } from './../../services/auth.service';
 
 const routes = new Router();
 
-routes.post('/signup', userController.Signup);
+routes.post('/signup', validate(userValidation.signup), userController.Signup);
 routes.post('/login', authLocalCustomer, userController.login);
+routes.post('/forget', validate(userValidation.forget), userController.forget);
+routes.post('/reset', validate(userValidation.reset), userController.resetPassword);
 routes.get('/oauth/google', (req, res, next) => {
   req.app.locals.role = req.params.role;
   next();
 }, authGoogle);
 routes.get('/oauth/google/redirect', authGoogle, userController.googleOAuth);
-// routes.get('/oauth/twitter', authTwitter);
-// routes.get('/oauth/twitter/redirect', authTwitter, userController.facebookOAuth);
-// routes.get('/oauth/facebook', authFacebook);
-// routes.get('/oauth/facebook/redirect', authFacebook, userController.facebookOAuth);
+routes.get('/oauth/twitter', (req, res, next) => {
+  req.app.locals.role = req.params.role;
+  next();
+}, authTwitter);
+routes.get('/oauth/twitter/redirect', authTwitter, userController.facebookOAuth);
+routes.get('/oauth/facebook', (req, res, next) => {
+  req.app.locals.role = req.params.role;
+  next();
+}, authFacebook);
+routes.get('/oauth/facebook/redirect', authFacebook, userController.facebookOAuth);
 export default routes;
